40b80ceb0a37c5a9baf6832d406ad6c66aab6199
V. Karras <v.karras98@gmail.com>
2025-01-28 16:24:34 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.ch1;
+
+/**
+ *  Prints Hello Coding Factory
+ *  in standar output (console)
+ *  @author karrovich
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints Hello Coding Factory
+
+        /*
+         * Prints in standard output
+         * bla bla bla bla
+         */
+        System.out.println("Hello Coding Factory!"); //Prints Hello World
+        System.out.print("Hello World");
+        System.out.println();
+    }
+}
+
